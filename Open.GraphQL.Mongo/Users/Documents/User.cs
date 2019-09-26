548373dadb75d48e132c0fd28d5815808413799e
Marcelo Belchior Dias <marcelo.belchior@outlook.com>
2019-09-26 15:26:24 -0300

    inicial

---
+++ Open.GraphQL.Mongo/Users/Documents/User.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open.GraphQL.Mongo.Users.Documents
+{
+  public  class User
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Salt { get; set; }
+        public DateTime Created { get; set; } 
+        public DateTime Update { get; set; }
+        public bool Active { get; set; }
+
+        public string ExternalAuth { get; set; }
+        public DateTime Birth { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Open.GraphQL.Mongo/Users/Repository/UserRepository.cs
@@ -0,0 +1,88 @@
+﻿using Autofac.Features.AttributeFilters;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Polly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Open.GraphQL.Mongo.Users.Repository
+{
+    public class UserRepository //: Domain.Repositories.IFavoritoRepository
+    {
+        private readonly string urlMongo;
+        private readonly IMongoDatabase mongoDatabase;
+        private readonly IConfigurationSection _appSettings;
+        private IAsyncPolicy _circuitBreaker;
+
+        private const string nomeCollection = "UserRepository";
+
+        public UserRepository(IConfigurationSection appSettings, [KeyFilter("User")]IAsyncPolicy circuitBreaker)
+        {
+            _appSettings = appSettings ?? throw new ArgumentNullException(nameof(appSettings));
+            _circuitBreaker = circuitBreaker;
+            urlMongo = appSettings.GetConnectionString("mongoUrl");
+
+            mongoDatabase = GenericMongo.Create(urlMongo);
+        }
+
+        public async Task<bool> Adicionar(Users.Documents.User favorito)
+        {
+            return await _circuitBreaker.ExecuteAsync<dynamic>(async () =>
+            {
+                var collection = mongoDatabase.GetCollection<Documents.User>(nomeCollection);
+
+                var filter = Builders<Documents.User>.Filter.Eq(s => s.Id, favorito.Id);
+                var update = Builders<Documents.User>.Update
+                                .Set(s => s.ClienteId, favorito.ClienteId)
+                                .Set(s => s.Sku, favorito.Sku);
+
+                UpdateResult actionResult = await collection.UpdateOneAsync(filter, update, new UpdateOptions() { IsUpsert = true });
+
+                return actionResult.IsAcknowledged;
+            });
+        }
+        public async Task<bool> Excluir(Favorito favorito)
+        {
+            return await _circuitBreaker.ExecuteAsync<dynamic>(async () =>
+            {
+                var collection = mongoDatabase.GetCollection<Documents.FavoritoDocument>(nomeCollection);
+                var filter = Builders<FavoritoDocument>.Filter.Eq(s => s.Sku, favorito.Sku);
+                var actionResult = await collection.DeleteOneAsync(filter);
+                return actionResult.IsAcknowledged;
+            });
+        }
+        public async Task<Favorito> GetBySKU(string clienteId, int sku)
+        {
+            var builder = Builders<Documents.FavoritoDocument>.Filter;
+            var condition = builder.Eq(p => p.ClienteId, clienteId);
+            condition = condition & builder.Eq(p => p.Sku, sku);
+            var item = await GetAllByCondition(condition);
+            return item.FirstOrDefault();
+        }
+
+        public async Task<IEnumerable<int>> GetAllByCliente(string clienteId)
+        {
+            var builder = Builders<Documents.FavoritoDocument>.Filter;
+            var condition = builder.Eq(p => p.ClienteId, clienteId);
+
+            return await _circuitBreaker.ExecuteAsync<dynamic>(async () =>
+            {
+                var collection = mongoDatabase.GetCollection<Documents.FavoritoDocument>(nomeCollection);
+                var result = await collection.FindAsync(condition);
+                return result.ToList().Select(s => s.Sku);
+            });
+        }
+
+        private async Task<IEnumerable<Favorito>> GetAllByCondition(FilterDefinition<Documents.FavoritoDocument> condition)
+        {
+            return await _circuitBreaker.ExecuteAsync<dynamic>(async () =>
+            {
+                var collection = mongoDatabase.GetCollection<Documents.FavoritoDocument>(nomeCollection);
+                var result = await collection.FindAsync(condition);
+                return result.ToList().Map();
+            });
+        }
+    }
+}
