38a7f020521b275b066baf46f8a82b834c0d7de8
Marcelo Belchior Dias <marcelo.belchior@outlook.com>
2019-10-03 18:07:21 -0300

    config

--- Open.GraphQL.DI/GraphQL.cs
+++ Open.GraphQL.DI/GraphQL.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using Autofac;
 using Autofac.Features.AttributeFilters;
+using Microsoft.Extensions.Configuration;
 using Open.GraphQL.Domain.Users.Interface;
 using Open.GraphQL.Mongo.Users.Repository;
 using Polly;
@@ -9,29 +11,35 @@ namespace Open.GraphQL.DI
 {
 
     public class Cliente : Module
+    {
+        protected override void Load(ContainerBuilder builder)
         {
-            protected override void Load(ContainerBuilder builder)
-            {
-                var ambiente = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var ambiente = System.Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
-               // var config = new ConfigurationBuilder()
+            var _dic = new Dictionary<string, string>();
+            _dic.Add("Development", "appSetting.Development.json");
+            _dic.Add("Production", "appSetting.Production.json");
+            IConfiguration configuration;
+            configuration.GetSection("Development");
+            var config = new ConfigurationBuilder().AddConfiguration(n
+           .AddConfiguration);
                //.SetBasePath(Directory.GetCurrentDirectory())
                //.AddJsonFile("appsettings.json")
                //.AddJsonFile(string.Format("appsettings.{0}.json", ambiente), optional: true);
 
-               // var configuration = config?.Build();
-               // builder.RegisterInstance(configuration);
+            // var configuration = config?.Build();
+            // builder.RegisterInstance(configuration);
 
-               // var canSource = new CancellationTokenSource();
-               // var can = canSource.Token;
+            // var canSource = new CancellationTokenSource();
+            // var can = canSource.Token;
 
-               // var _appSettings = configuration.GetSection("configuracao");
+            // var _appSettings = configuration.GetSection("configuracao");
 
-               // builder.RegisterInstance(_appSettings).As<IConfigurationSection>();
+            // builder.RegisterInstance(_appSettings).As<IConfigurationSection>();
 
             //    builder.Register((c, p) => new LoggerFactory().AddLog4Net("log4net.config").CreateLogger("Cliente")).As(typeof(Microsoft.Extensions.Logging.ILogger)).SingleInstance();
 
-                builder.RegisterInstance(
+            builder.RegisterInstance(
                     Policy
                     .Handle<Exception>()
                     .CircuitBreakerAsync(
@@ -39,57 +47,57 @@ namespace Open.GraphQL.DI
                         durationOfBreak: TimeSpan.FromSeconds(30))
                     ).As<IAsyncPolicy>().Keyed<IAsyncPolicy>("User");
 
-                //builder.RegisterInstance(
-                //    Policy
-                //    .Handle<Exception>()
-                //    .CircuitBreakerAsync(
-                //        exceptionsAllowedBeforeBreaking: 10,
-                //        durationOfBreak: TimeSpan.FromSeconds(30))
-                //    ).As<IAsyncPolicy>().Keyed<IAsyncPolicy>("Pedido");
+            //builder.RegisterInstance(
+            //    Policy
+            //    .Handle<Exception>()
+            //    .CircuitBreakerAsync(
+            //        exceptionsAllowedBeforeBreaking: 10,
+            //        durationOfBreak: TimeSpan.FromSeconds(30))
+            //    ).As<IAsyncPolicy>().Keyed<IAsyncPolicy>("Pedido");
 
-                //var retryOMS = Policy
-                //   .Handle<Exception>()
-                //   .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(2))
-                //   .WrapAsync(Policy.Handle<Exception>().CircuitBreakerAsync(
-                //               exceptionsAllowedBeforeBreaking: 10,
-                //               durationOfBreak: TimeSpan.FromSeconds(30)));
+            //var retryOMS = Policy
+            //   .Handle<Exception>()
+            //   .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(2))
+            //   .WrapAsync(Policy.Handle<Exception>().CircuitBreakerAsync(
+            //               exceptionsAllowedBeforeBreaking: 10,
+            //               durationOfBreak: TimeSpan.FromSeconds(30)));
 
-                //var retryCliente = Policy
-                //   .Handle<Exception>()
-                //   .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(2))
-                //   .WrapAsync(Policy.Handle<Exception>().CircuitBreakerAsync(
-                //               exceptionsAllowedBeforeBreaking: 10,
-                //               durationOfBreak: TimeSpan.FromSeconds(30)));
-                //var retryEntrega = Policy
-                //   .Handle<Exception>()
-                //   .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(2))
-                //   .WrapAsync(Policy.Handle<Exception>().CircuitBreakerAsync(
-                //               exceptionsAllowedBeforeBreaking: 10,
-                //               durationOfBreak: TimeSpan.FromSeconds(30)));
+            //var retryCliente = Policy
+            //   .Handle<Exception>()
+            //   .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(2))
+            //   .WrapAsync(Policy.Handle<Exception>().CircuitBreakerAsync(
+            //               exceptionsAllowedBeforeBreaking: 10,
+            //               durationOfBreak: TimeSpan.FromSeconds(30)));
+            //var retryEntrega = Policy
+            //   .Handle<Exception>()
+            //   .WaitAndRetryAsync(3, i => TimeSpan.FromSeconds(2))
+            //   .WrapAsync(Policy.Handle<Exception>().CircuitBreakerAsync(
+            //               exceptionsAllowedBeforeBreaking: 10,
+            //               durationOfBreak: TimeSpan.FromSeconds(30)));
 
-                //builder.Register((c, p) => new Framework.Servico.HttpHelper(retryOMS)).As<Framework.Servico.IHttpHelper>().Keyed<Framework.Servico.IHttpHelper>("OMS").SingleInstance();
-                //builder.Register((c, p) => new Framework.Servico.HttpHelper(retryCliente)).As<Framework.Servico.IHttpHelper>().Keyed<Framework.Servico.IHttpHelper>("Cliente").SingleInstance();
-                //builder.Register((c, p) => new Framework.Servico.HttpHelper(retryEntrega)).As<Framework.Servico.IHttpHelper>().Keyed<Framework.Servico.IHttpHelper>("Entrega").SingleInstance();
+            //builder.Register((c, p) => new Framework.Servico.HttpHelper(retryOMS)).As<Framework.Servico.IHttpHelper>().Keyed<Framework.Servico.IHttpHelper>("OMS").SingleInstance();
+            //builder.Register((c, p) => new Framework.Servico.HttpHelper(retryCliente)).As<Framework.Servico.IHttpHelper>().Keyed<Framework.Servico.IHttpHelper>("Cliente").SingleInstance();
+            //builder.Register((c, p) => new Framework.Servico.HttpHelper(retryEntrega)).As<Framework.Servico.IHttpHelper>().Keyed<Framework.Servico.IHttpHelper>("Entrega").SingleInstance();
 
-                //builder.RegisterType<Application.Services.FavoritoService>().As<Application.Services.IFavoritoService>().SingleInstance().WithAttributeFiltering();
-                //builder.RegisterType<MongoDB.Cliente.Repositories.FavoritoRepository>().As<Domain.Repositories.IFavoritoRepository>().SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<Application.Services.FavoritoService>().As<Application.Services.IFavoritoService>().SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<MongoDB.Cliente.Repositories.FavoritoRepository>().As<Domain.Repositories.IFavoritoRepository>().SingleInstance().WithAttributeFiltering();
 
-                builder.RegisterType<UserRepository>().As<IUserRepository>().SingleInstance().WithAttributeFiltering();
+            builder.RegisterType<UserRepository>().As<IUserRepository>().SingleInstance().WithAttributeFiltering();
 
 
-                //builder.RegisterType<Application.Reagendamento.Service.ReagendamentoService>().As<Application.Reagendamento.Services.IReagendamentoService>().SingleInstance().WithAttributeFiltering();
-                //builder.RegisterType<Http.Entrega.Repositories.TokStokRepository>().As<Domain.Reagendamento.ITokStokRepository>().SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<Application.Reagendamento.Service.ReagendamentoService>().As<Application.Reagendamento.Services.IReagendamentoService>().SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<Http.Entrega.Repositories.TokStokRepository>().As<Domain.Reagendamento.ITokStokRepository>().SingleInstance().WithAttributeFiltering();
 
-                //builder.RegisterType<Application.Services.PedidoService>().As<Application.Services.IPedidoService>().SingleInstance().WithAttributeFiltering();
-                //builder.RegisterType<MongoDB.Pedido.Repositories.PedidoRepository>().As<Domain.Repositories.IPedidoRepository>().Keyed<Domain.Repositories.IPedidoRepository>("DB").SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<Application.Services.PedidoService>().As<Application.Services.IPedidoService>().SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<MongoDB.Pedido.Repositories.PedidoRepository>().As<Domain.Repositories.IPedidoRepository>().Keyed<Domain.Repositories.IPedidoRepository>("DB").SingleInstance().WithAttributeFiltering();
 
-                //builder.RegisterType<Http.Repositories.VtexRepository>().As<Domain.Repositories.IPedidoRepository>().Keyed<Domain.Repositories.IPedidoRepository>("HTTP").SingleInstance().WithAttributeFiltering();
-                //builder.RegisterType<Http.Repositories.VtexClienteRepository>().As<Domain.Repositories.IClienteRepository>().SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<Http.Repositories.VtexRepository>().As<Domain.Repositories.IPedidoRepository>().Keyed<Domain.Repositories.IPedidoRepository>("HTTP").SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<Http.Repositories.VtexClienteRepository>().As<Domain.Repositories.IClienteRepository>().SingleInstance().WithAttributeFiltering();
 
-                //builder.RegisterType<Application.Cliente.Services.ClienteService>().As<Application.Cliente.Services.IClienteService>().SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<Application.Cliente.Services.ClienteService>().As<Application.Cliente.Services.IClienteService>().SingleInstance().WithAttributeFiltering();
 
-                //builder.RegisterType<MongoDB.MongoHealthCheck>().As<IHealthCheck>().SingleInstance().WithAttributeFiltering();
+            //builder.RegisterType<MongoDB.MongoHealthCheck>().As<IHealthCheck>().SingleInstance().WithAttributeFiltering();
 
-            }
         }
     }
+}
