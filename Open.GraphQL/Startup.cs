03ae7f5ba920cea1730d0104844322947398a4db
Marcelo Belchior Dias <marcelo.belchior@outlook.com>
2019-10-09 12:05:04 -0300

    fix - Ajustment carrocel

--- Open.GraphQL/Startup.cs
+++ Open.GraphQL/Startup.cs
@@ -1,21 +1,57 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using GraphQL.Server;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.Extensions.Http;
+using System;
+using System.Net.Http;
 
 namespace Open.GraphQL
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+
+        private readonly IHostingEnvironment _env;
+        private readonly IConfiguration _config;
+
+
+        private static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+            
+            //services.AddMvc()
+            //    .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+            //    .AddJsonOptions(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
 
+            ////services.AddHealthChecks();
+
+            //services.AddHealthChecks()
+            //      .AddCheck<MongoDB.MongoHealthCheck>("mongodb",
+            //                                   failureStatus: HealthStatus.Unhealthy,
+            //                                   tags: new[] { "elasticsearch" });
+
+
+
+
+
+            //services.AddSwaggerGen(s => {
+            //    s.SwaggerDoc("v1", new Info { Title = "Catalogo.WebApi", Version = "V1" });
+            //    s.EnableAnnotations();
+            //});
+
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
