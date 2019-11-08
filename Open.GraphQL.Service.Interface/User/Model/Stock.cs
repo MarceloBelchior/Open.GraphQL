90526a7edf79e7f7da281996696a74532fef0381
Marcelo Belchior Dias <marcelo.belchior@outlook.com>
2019-11-08 18:41:39 -0300

    add stock and user

---
+++ Open.GraphQL.Service.Interface/User/Model/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open.GraphQL.Service.Interface.User.Model
+{
+   public class Stock
+    {
+        public string code { get; set; }
+        public int quantity { get; set; }
+        public decimal pay { get; set; }
+        public decimal tax { get; set; }
+        public DateTime buywhen { get; set; }
+
+        public decimal profit { get; set; }
+       
+        public string information { get; set; }
+
+    }
+}
---
+++ Open.GraphQL.Service.Interface/User/Model/account_info.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open.GraphQL.Service.Interface.User.Model
+{
+   public class account_info
+    {
+        public string nick { get; set; }
+        public string card { get; set; }
+        public bool active { get; set; }
+        
+
+    }
+}
